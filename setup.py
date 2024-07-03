de15b5a464ca8ac577667fd3af97d955d5bc6f2e
Brian Edward <inph1del@yahoo.com>
2024-07-03 09:32:10 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='CaptainsMate',
+    version='0.1',
+    packages=['CaptainsMate'],
+    install_requires=[
+        'discord==2.3.2',
+        'discord.py==2.3.2',
+        'aiohttp==3.9.3',
+        'multidict==6.0.5',
+        'yarl==1.9.4'
+    ]
+)
